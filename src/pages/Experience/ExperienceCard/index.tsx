f0e8c83bbb2479cb645f5c6855b8f02115ba642f
Channy <117459099+channyhow@users.noreply.github.com>
2023-09-22 22:03:32 +0200

    last update

--- src/pages/Experience/ExperienceCard/index.tsx
+++ src/pages/Experience/ExperienceCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
-import { Experience } from '../../@types';
+import { Experience } from '../../../@types';
 
 interface ExperienceCardProps {
   experience: Experience;
