86c546fa4c4465e72abd575e38b7435edfa96a21
Channy <117459099+channyhow@users.noreply.github.com>
2023-09-29 15:32:14 +0200

    pepette

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,4 +1,6 @@
 import './styles.scss';
+import Pepe from '../../assets/pepe.png';
+import Pepette from '../../assets/sticker_16.png';
 
 function Error() {
   return (
@@ -6,6 +8,8 @@ function Error() {
       <h1>Whoops</h1>
       <p>seems like this page doesn't exist!</p>
       <p>how about going home?</p>
+      <img src={Pepette} alt="Pepe" style={{ scale: '50%' }} />
+
     </div>
   );
 }
