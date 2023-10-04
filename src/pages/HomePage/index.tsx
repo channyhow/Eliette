fb2b084ff87fed89ef0edf4e5070e670fc04d588
Channy <117459099+channyhow@users.noreply.github.com>
2023-10-04 17:13:43 +0200

    frfrg

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -5,13 +5,14 @@ import './styles.scss';
 function HomePage() {
   return (
 
-    <div className="home">
+    <div>
       {' '}
       <motion.main
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 3 }}
+        className="home"
       >
         <Info />
       </motion.main>
