0d7078953bc4f8ae1dcb2fd9a4f967c7e9f576d9
Channy <117459099+channyhow@users.noreply.github.com>
2024-03-15 20:59:05 +0100

    projects

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import './styles.scss';
-import QuiltedImageList from '../../components/ImageList';
-import { JLALBANY_IMAGE_MAP, SASHAYOGAFLOW_IMAGE_MAP } from '../../utils/imageMap';
 import Cards from '../../components/Card';
-import portfolio from './../../utils/data/portfolio.json';
-function Projects() {
-  // Combine and transform the image maps into the required array format
-  const combinedImageData = [
-    ...Object.values(JLALBANY_IMAGE_MAP),
-    ...Object.values(SASHAYOGAFLOW_IMAGE_MAP),
-  ].map((image) => ({
-    src: image.src,
-    alt: image.alt,
-    cols: image.cols || 1, // Default to 1 if not specified
-    rows: image.rows || 1, // Default to 1 if not specified
-  }));
+import portfolio from '../../utils/data/portfolio.json';
 
+function Projects() {
   return (
     <motion.div
       initial={{ opacity: 0, y: 0 }}
@@ -24,8 +12,7 @@ function Projects() {
       transition={{ duration: 0.5 }}
       className="projects"
     >
-      <Cards data={portfolio}/>
-      {/* <QuiltedImageList data={combinedImageData} /> */}
+      <Cards data={portfolio} />
     </motion.div>
   );
 }
